7732ec2d529367e8c94491f6b71d62c6e5a8b24e
Salma Yasmin <salmayasmin93@yahoo.com>
2025-04-10 10:20:07 -0400

    backend update

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'http://localhost:5001'
+  API_URL: process.env.REACT_APP_API_URL || 'https://ai-symptom-checker-3sr4.onrender.com'
 };
 
 export default config; 
